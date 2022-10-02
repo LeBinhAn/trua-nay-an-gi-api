66c6492dc086ec61a2e96383d7d5067b15e1d2e8
Le Binh An <lvba2102@gmail.com>
2022-10-02 21:08:56 +0700

    Add build script

--- index.ts
+++ index.ts
@@ -5,9 +5,19 @@ const app: Express = express();
 const port = 8000;
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('Express + TypeScript Server')
-})
+	res.send('Express + TypeScript Server');
+});
+
+app.get('/dish', (req: Request, res: Response) => {
+  const dish = {
+    name: "Mi 🍜",
+    price: "35000",
+    currency: "VND",
+  };
+
+  res.send(dish)
+});
 
 app.listen(port, () => {
-	console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
-})
+	console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
